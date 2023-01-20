9bc80adc84c5b00fbb0373940f8cf7c850403c24
Kylaaa <kyler.mulherin@gmail.com>
2023-01-20 10:01:08 -0500

    Twitch Extension code

---
+++ TwitchExtension/local_debug/installTestingCertificate.ps1
@@ -0,0 +1,5 @@
+# if you have the Go programming language installed, you
+# can replace 'mkcert' in the following line with go run github.com/FiloSottile/mkcert
+# to automatically download, install and run it.
+mkcert -install localhost
+# > The certificate is at "./localhost.pem" and the key at "./localhost-key.pem"
